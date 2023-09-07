4d826ad347fd6bbddc0f248ebd6ce9604a9751fe
ANK <kalupahanaadheetha5@gmail.com>
2023-09-07 07:16:05 +0530

    complete navigation in main form for table forms for reservation & room

--- src/main/java/controller/MenuFormController.java
+++ src/main/java/controller/MenuFormController.java
@@ -3,6 +3,7 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -13,8 +14,13 @@ import util.NewWindowUI;
 import util.Routes;
 
 import java.io.IOException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
 
-public class MenuFormController {
+public class MenuFormController implements Initializable {
 
     @FXML private AnchorPane tblPane;
     @FXML private AnchorPane pane;
@@ -22,6 +28,11 @@ public class MenuFormController {
     @FXML private JFXButton btnRoom;
     @FXML private JFXButton btnReservation;
     @FXML private Label lblDate;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setDateAndTime();
+    }
 
     @FXML
     void btnReservationOnAction(ActionEvent event) {
@@ -65,12 +76,22 @@ public class MenuFormController {
 
     @FXML
     void imgtblReservedOnMouseClicked(MouseEvent event) {
-        System.out.println("it works! :0");
+        try {
+            Navigation.navigate(Routes.ReservationTableForm,tblPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"UI not found!").show();
+        }
     }
 
     @FXML
     void imgtblRoomOnMouseClicked(MouseEvent event) {
-        System.out.println("it works! :0");
+        try {
+            Navigation.navigate(Routes.RoomTableForm,tblPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"UI not found!").show();
+        }
     }
 
     @FXML
@@ -81,6 +102,18 @@ public class MenuFormController {
             e.printStackTrace();
             new Alert(Alert.AlertType.ERROR,"UI not found!").show();
         }
+    }
+
+    private void setDateAndTime(){
+        // 1. get date/time
+        Date currentDateTime = new Date();
+
+        // 2. get Date with Zone & AM/PM marker
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a z");
+        String formatterDateTime = dateFormat.format(currentDateTime);
+
+        //set value to lbl
+        lblDate.setText(formatterDateTime);
     }
 
 }
--- src/main/java/controller/RoomTableFormController.java
+++ src/main/java/controller/RoomTableFormController.java
@@ -1,4 +1,10 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class RoomTableFormController {
+
+    @FXML private AnchorPane pane;
+
 }
